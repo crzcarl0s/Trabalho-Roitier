1242d76e959c19ae74a85e9f23df633350ba1885
YAN-Ks <ydiasdacruz@gmail.com>
2025-05-06 17:17:29 -0300

    Delete entrypoint.sh

--- ftp/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Garante que o diretório do usuário exista
-mkdir -p /home/ftpuser/ftp/files
-chown -R ftpuser:ftpuser /home/ftpuser/ftp
-
-# Inicia o serviço FTP
-vsftpd /etc/vsftpd.conf
