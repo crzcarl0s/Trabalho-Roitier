6cb0d53349fe6c14f3d68083bf2722aac170eafb
YAN-Ks <ydiasdacruz@gmail.com>
2025-05-06 17:18:01 -0300

    Delete entrypoint.sh

--- samba/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cria o usuário joao (deve já existir no LDAP para funcionar com integração real)
-adduser --no-create-home --disabled-password --gecos "" joao
-(echo senha123; echo senha123) | smbpasswd -a joao
-
-# Inicia o serviço
-smbd -F
